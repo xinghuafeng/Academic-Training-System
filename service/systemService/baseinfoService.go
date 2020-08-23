1482c24244e37540b726223e3fb03508f852d40e
xinghuafeng <381799493@qq.com>
2020-08-23 15:37:49 +0800

    修改校区信息列表分页

--- service/systemService/baseinfoService.go
+++ service/systemService/baseinfoService.go
@@ -4,16 +4,19 @@ import (
 	"Academic-Training-System/db"
 	"Academic-Training-System/model/SystemModel"
 	"github.com/gin-gonic/gin"
+	"strconv"
 )
 
 func All(ctx *gin.Context) ([]SystemModel.Baseinfo, error) {
 	model := make([]SystemModel.Baseinfo, 0)
 	var err error
 	param := ctx.Query("param")
+	pageSize, _ := strconv.Atoi(ctx.Query("pageSize"))
+	pageNo, _ := strconv.Atoi(ctx.Query("pageNo"))
 	if "" == param {
-		err = db.Engine.Find(&model)
+		err = db.Engine.Limit(pageSize, (pageNo-1)*pageSize).Find(&model)
 	} else {
-		err = db.Engine.Where("basename = ?", param).Find(&model)
+		err = db.Engine.Where("basename = ?", param).Limit(pageSize, (pageNo-1)*pageSize).Find(&model)
 	}
 
 	return model, err
